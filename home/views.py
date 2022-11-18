9025f7aed985516f04343881f2c44074fd1b973a
Ankit Rai <raia85859@gmail.com>
2022-11-18 21:13:15 +0545

    views

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .views import *
+from .models import *
 from django.views.generic import View
 # Create your views here.
 
@@ -11,4 +12,13 @@ class BaseView(View):
 class HomeView(BaseView):
 
     def get(self,request):
-        self.context['categories'] = Cate
+        self.context['categories'] = Category.objects.all()
+        self.context['subcategories'] = SubCategory.objects.all()
+        self.context['sliders'] = Slider.objects.all()
+        self.context['brand'] = Brand.objects.all()
+        self.context['ads'] = Ad.objects.all()
+        self.context['news'] = Product.objects.filter(labels='new')
+        self.context['hots'] = Product.objects.filter(labels ='hot')
+        self.context['reviews'] =Reviews.objects.all()
+
+        return render(request,'index.html',self.context)
